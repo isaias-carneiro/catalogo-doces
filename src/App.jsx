4611f021372ce307908cfe1939bf4d916c40b738
Isaias Carneiro <isa.adiventure@gmail.com>
2026-02-01 16:00:15 -0300

    Ajuste tamanho das imagens

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,53 @@
 import "./App.css";
 import { Phone, Heart } from "lucide-react";
+import aplique from "./assets/Doces_Aplique.jpeg";
+import frutinhas from "./assets/Doces_Frutinhas.jpeg";
+import base from "./assets/Doces_detalhe_Base.jpeg";
+import d2 from "./assets/Doces_Modelado_2D_Base.jpeg";
+import d3 from "./assets/Doces_Modelado_3D_Base.jpeg";
+import grandes from "./assets/Doces_Grandes.jpeg";
+
 
 export default function App() {
   const doces = [
     {
       nome: "Apliques",
       preco: "R$ 2,00",
-      descricao: "Apliques comestíveis personalizados para decorar seu bolo"
+      descricao: "Apliques comestíveis personalizados",
+      imagem: aplique,
     },
     {
       nome: "Frutinhas",
       preco: "R$ 2,00",
-      descricao: "Frutinhas de leite para decorar sua mesa"
+      descricao: "Frutinhas de leite artesanais",
+      imagem: frutinhas,
     },
     {
-      nome: "Doces Detalhados na Base",
+      nome: "Detalhados na Base",
       preco: "R$ 2,50",
-      descricao: "Detalhe e base feitas de leite"
+      descricao: "Detalhe e base feitas de leite",
+      imagem: base,
     },
     {
-      nome: "Doces Modelados 2D na Base",
+      nome: "Modelados 2D",
       preco: "R$ 3,00",
-      descricao: "Modelados em relevo com acabamento fino"
+      descricao: "Modelados em relevo",
+      imagem: d2,
     },
     {
-      nome: "Doces Modelados 3D sem Base",
+      nome: "Modelados 3D",
       preco: "R$ 3,50",
-      descricao: "Esculturas em 3D feitas à mão"
+      descricao: "Esculturas em 3D",
+      imagem: d3,
     },
     {
-      nome: "Doces Versões Maiores",
+      nome: "Versões Maiores",
       preco: "A partir de R$ 8,00",
-      descricao: "Valor a negociar diretamente"
-    }
+      descricao: "Valor a negociar",
+      imagem: grandes,
+    },
   ];
+  
 
   return (
     <div className="container">
@@ -51,16 +65,25 @@ export default function App() {
       <main className="cards">
         {doces.map((doce, index) => (
           <div key={index} className="card">
-            <h2>{doce.nome}</h2>
 
-            <p className="descricao">
-              {doce.descricao}
-            </p>
-
-            <span className="preco">
-              {doce.preco}
-            </span>
-          </div>
+          <img
+            src={doce.imagem}
+            alt={doce.nome}
+            className="foto"
+          />
+        
+          <h2>{doce.nome}</h2>
+        
+          <p className="descricao">
+            {doce.descricao}
+          </p>
+        
+          <span className="preco">
+            {doce.preco}
+          </span>
+        
+        </div>
+        
         ))}
       </main>
 
